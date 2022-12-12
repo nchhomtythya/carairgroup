051c9767d93371ebd6a9434f239cfa8b5b1ea40f
nchhomtythya <nctythya@outlook.com>
2022-12-12 19:23:40 +1100

    db schema

--- db/schema.rb
+++ db/schema.rb
@@ -68,9 +68,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_08_213211) do
     t.string "colour"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
     t.float "latitude"
     t.float "longitude"
-    t.string "address"
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
